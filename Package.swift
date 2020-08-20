91cf536543ed083f1f2676fa08727a302b935623
Oliver Ian Krakora <oliver.krakora@allaboutapps.at>
2020-08-20 22:37:13 +0200

    added spm support

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SVProgressHUD",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "SVProgressHUD",
+            targets: ["SVProgressHUD"])
+    ],
+    targets: [
+        .target(name: "SVProgressHUD",
+                path: "SVProgressHUD",
+                exclude: ["Supporting Files",
+                          "SVProgressHUD.bundle"],
+                resources: [.copy("SVProgressHUD.bundle")])
+    ]
+)
